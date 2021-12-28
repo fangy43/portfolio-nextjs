dcc161921ba7b9fd38d44d57f73d6bdbe075ceb9
Nattarat Serirat <fangy2543@hotmail.com>
2021-12-28 14:08:30 +0700

    fig error

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -5,28 +5,28 @@ export const Footer = () => {
             <div className={styles.title}>Contact</div>
             <div className={styles.contactList}>
                 <div className={styles.contactItem}>
-                <i class="fa fa-phone"></i>
+                <i className="fa fa-phone"></i>
                     <div className={styles.details}>
                         <div className={styles.method}>Tel</div>
                         <div className={styles.data}>080-2848185</div>
                     </div>
                 </div>
                 <div className={styles.contactItem}>
-                    <i class="fa fa-globe"></i>
+                    <i className="fa fa-globe"></i>
                     <div className={styles.details}>
                         <div className={styles.method}>E-mail</div>
                         <div className={styles.data}>nattarat.srr@gmail.com</div>
                     </div>
                 </div>
                 <div className={styles.contactItem}>
-                    <i class="fa fa-facebook-square"></i>
+                    <i className="fa fa-facebook-square"></i>
                     <div className={styles.details}>
                         <div className={styles.method}>Facebook</div>
                         <div className={styles.data}>Nattarat Serirat</div>
                     </div>
                 </div>
                 <div className={styles.contactItem}>
-                    <i class="fa fa-github"></i>
+                    <i className="fa fa-github"></i>
                     <div className={styles.details}>
                         <div className={styles.method}>Github</div>
                         <div className={styles.data}>fangy43</div>
