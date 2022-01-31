97417334d3e62ba490f7c03a39f0a96a88bef264
Nattarat Serirat <fangy2543@hotmail.com>
2022-01-31 14:22:31 +0700

    fix deploy error

--- components/Education/Education.jsx
+++ components/Education/Education.jsx
@@ -32,9 +32,9 @@ export const Education = ({ darkMode }) => {
                             date={ed.period}
                             dateClassName={styles.date}
                             iconStyle={{ background:'#fda517' ,boxShadow:!darkMode &&'0 0 0 4px #fda517,inset 0 0 1px rgba(255,255,255,0.8), 3px 10px 5px rgba(0, 0, 0, 0.1)'}}
-                            icon={<div className={styles.timelineIcon}><i class="fa fa-graduation-cap" style={{fontSize:'40px',color:"white"}}></i></div>}
+                            icon={<div className={styles.timelineIcon}><i className="fa fa-graduation-cap" style={{fontSize:'40px',color:"white"}}></i></div>}
                             contentStyle={{ borderTop: '7px solid #fda517', boxShadow: !darkMode && '0px 0px 12px #cdcdcd' }}
-                       
+                            key={ed.title}
                         >
                             <h2 className="vertical-timeline-element-title">{ed.title}</h2>
                             <h4 className="vertical-timeline-element-subtitle">{ed.subtitle}</h4>
