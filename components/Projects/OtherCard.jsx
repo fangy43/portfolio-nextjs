b86bfd775e2605e0a4cd110ae98a0a4b2a596f2e
Nattarat Serirat <fangy2543@hotmail.com>
2022-03-04 17:27:06 +0700

    fix animation bug

--- components/Projects/OtherCard.jsx
+++ components/Projects/OtherCard.jsx
@@ -6,19 +6,21 @@ export const OtherCard = ({ project, darkMode }) => {
     return (
         <a href={project.html_url} target="_blank" rel="noreferrer">
             <motion.div
-                className={css(darkMode ? styles.card : styles.lightCard)}
-                initial={{ opacity:0,y:40 }}
+                initial={{ opacity: 0, y: 40 }}
                 whileInView={{ opacity: [0, 1], y: [50, 0] }}
-                transition={{ duration:1,type: "spring", bounce: 0.5}}
+                transition={{ duration: 0.75, type: "spring", bounce: 0.5, }}
                 viewport={{ once: true }}
             >
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
-                    width={50}
-                />
-                <div className={styles.textbox}>
-                    <div className={styles.title}>{project.name}</div>
-                    <div className={styles.description}>{project.description}</div>
+                <div
+                    className={css(darkMode ? styles.card : styles.lightCard)}>
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
+                        width={50}
+                    />
+                    <div className={styles.textbox}>
+                        <div className={styles.title}>{project.name}</div>
+                        <div className={styles.description}>{project.description}</div>
+                    </div>
                 </div>
             </motion.div>
         </a>
--- components/Projects/ProjectCard.jsx
+++ components/Projects/ProjectCard.jsx
@@ -5,24 +5,26 @@ import { motion } from 'framer-motion'
 export const ProjectCard = ({ project, darkMode, index }) => {
     return (
         <motion.div
-            className={css(darkMode ? styles.card : styles.lightCard)}
-            initial={{ opacity:0,y:50 }}
-            whileInView={{ opacity:1,y:0 }}
-            transition={{duration:1.5,delay:index-0.5,type: "spring", bounce: 0.5}}
-            viewport={{once:true }}
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.5, delay: index - 0.5, type: "spring", bounce: 0.5 }}
+            viewport={{ once: true }}
         >
-            <div className={styles.imagebox}>
-                <img className={styles.img} src={project.img} />
-            </div>
-            <div className={styles.textbox}>
-                <div className={styles.title}>{project.title}</div>
-                <div className={styles.period}>{project.period}</div>
-                <div className={styles.description}>{project.description}</div>
-            </div>
-            <div className={styles.link}>
-                <a href={project.link} target="_blank" rel="noreferrer">
-                    <button className={styles.btn}>See Github</button>
-                </a>
+            <div
+                className={css(darkMode ? styles.card : styles.lightCard)}>
+                <div className={styles.imagebox}>
+                    <img className={styles.img} src={project.img} />
+                </div>
+                <div className={styles.textbox}>
+                    <div className={styles.title}>{project.title}</div>
+                    <div className={styles.period}>{project.period}</div>
+                    <div className={styles.description}>{project.description}</div>
+                </div>
+                <div className={styles.link}>
+                    <a href={project.link} target="_blank" rel="noreferrer">
+                        <button className={styles.btn}>See Github</button>
+                    </a>
+                </div>
             </div>
         </motion.div >
     )
