037c0ba328fe763b7715427efa2a2c600be1d743
Nattarat Serirat <fangy2543@hotmail.com>
2022-03-09 18:03:41 +0700

    add preview

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
       <Head>
         <title>Fangy</title>
         <meta name="description" content="Nattarat Serirat's Portfolio" />
+        <meta property="og:image" content="/preview-img.png" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
